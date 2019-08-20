f6529461b4a63d474d5515dbcf9df52ae1154d48
Terashima <yamanogomora@gmail.com>
2019-08-20 14:22:08 +0900

    Add: メモを追加する画面を作成

---
+++ SimpleMemoApp/MemoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MemoViewController.swift
+//  SimpleMemoApp
+//
+//  Created by 寺島 洋平 on 2019/08/20.
+//  Copyright © 2019年 YoheiTerashima. All rights reserved.
+//
+
+import UIKit
+
+class MemoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
